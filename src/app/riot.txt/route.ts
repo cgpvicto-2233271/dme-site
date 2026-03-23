27d8479e7c640ff1dc6c41cbb5e4763d70dd0717
Mathieu Cousança <dc61789@gmail.com>
2026-03-23 19:40:38 -0400

    final

--- src/app/riot.txt/route.ts
+++ src/app/riot.txt/route.ts
@@ -1,5 +1,5 @@
 export function GET() {
-  return new Response("F88952aeA055-42D5-89F8-7A01B37621AC", {
+  return new Response("f88952ae-a055-42d5-89f8-7a01b37621ac", {
     headers: { "Content-Type": "text/plain; charset=utf-8" },
   });
 }
